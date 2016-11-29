df93500f7dba246b4da9a0fb615c154240993b12
楊博名 <AndrewHowCool@yangbomingde-MacBook-Air.local>
2016-11-29 15:08:00 +0800

    Initial Commit

---
+++ Defeat_Cour/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Defeat_Cour
+//
+//  Created by 楊博名 on 2016/11/29.
+//  Copyright © 2016年 楊博名. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
